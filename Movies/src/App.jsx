625c35fd5ea38afbdebd21afd650f12ee5e164dc
RaulRv007 <116015516+RaulRv007@users.noreply.github.com>
2025-01-25 02:14:20 +0100

    first commit

---
+++ Movies/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <p>Hello World</p>
+      <Text />
+      <Text />
+    </div>
+
+  )
+}
+
+function Text() {
+  return (
+    <div>
+      <p>Text</p>
+    </div>
+  )
+}
+
+export default App
